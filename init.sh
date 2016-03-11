47919033e1dbe85c0d830a3c0bbf4de108de0077
valentinCro <valentin.crochemore1@etu.univ-rouen.fr>
2016-03-11 10:47:15 +0100

    fix (init.sh) : fix java install

--- init.sh
+++ init.sh
@@ -21,12 +21,14 @@ sudo apt-get --yes --force-yes install terminator
 sudo apt-get --yes --force-yes install software-center
 sudo apt-get --yes --force-yes install firefox
 
+
 #Install JAVA
-sudo echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee /etc/apt/sources.list.d/webupd8team-java.list
-sudo echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
+sudo echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | sudo tee /etc/apt/sources.list.d/java-8-debian.list
+sudo echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | sudo tee -a /etc/apt/sources.list.d/java-8-debian.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
 sudo apt-get update
-sudo apt-get install oracle-java8-installer
+sudo apt-get --yes --force-yes install oracle-java8-installer
+sudo apt-get --yes --force-yes install oracle-java8-set-default
 
 #install composer
 cd ~/
